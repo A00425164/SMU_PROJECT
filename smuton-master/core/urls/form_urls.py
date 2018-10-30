a17d377d46be853da422160064bed4ecf250a28f
A00425164 <40704271+A00425164@users.noreply.github.com>
2018-10-29 22:37:48 -0300

    working on retrieving from DB

--- smuton-master/core/urls/form_urls.py
+++ smuton-master/core/urls/form_urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from ..views.form_views import SumbitFormView
-from ..views.form_views import DynamicFormView
+from ..views.form_views import DynamicFormView1
+from ..views.form_views import DynamicFormView2
 from django.contrib.auth.decorators import login_required
 
 
@@ -9,7 +10,12 @@ urlpatterns = [
         login_required(SumbitFormView.as_view()),
         name="hackathon_form"),
 		
-	 url(r'^dynamic/$',
-         login_required(DynamicFormView.as_view()),
+	 url(r'^dynamic1/$',
+         login_required(DynamicFormView1.as_view()),
          name="DynamicFormPage1"),
+         
+    url(r'^dynamic2/$',
+         login_required(DynamicFormView2.as_view()),
+         name="DynamicFormPage2"),
+  
 ]
--- smuton-master/core/views/form_views.py
+++ smuton-master/core/views/form_views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 from django.views import View
 from core.forms import DynamicForm
 from django.template.context_processors import request
+from core import models
+#from django.views.generic.edit import CreateView
 
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from ..models import Criteria
+from ..forms import CriteriaForm
+from django.urls import reverse_lazy
+from django.urls import reverse
+from django.http import Http404
 
 class SumbitFormView(View):
     template_name = "core/InitialFormPage1.html"
@@ -14,8 +24,9 @@ class SumbitFormView(View):
     def dynamicformpage1(self, request, *args, **kwargs):
         return render(request, 'DynamicFormPage1.html')
     
-class DynamicFormView(View):
+class DynamicFormView1(View):#CreateView? Check all available views  try
     template_name = "core/DynamicFormPage1.html"
+    
     
     def get(self, request, *args, **kwargs):
         form = DynamicForm()
@@ -33,7 +44,83 @@ class DynamicFormView(View):
             
         
         args = {'form': form, 'data':data}
+        models.Criteria.objects.all()
+        models.Scale.objects.all()
         print(request.POST)
         return render(request, self.template_name, args)
         #return render(request, self.template_name, answer)
         #return super(DynamicFormView, self).post(request, *args, **kwargs)
+        
+class DynamicFormView2(CreateView):#CreateView? Check all available views  try
+    template_name = "core/DynamicFormPage2.html"
+    model = Criteria
+    form_class = DynamicForm
+    success_url = reverse_lazy("dynamic2")
+    
+    
+    def __init__(self, **kwargs):
+        return super(DynamicFormView2, self).__init__(**kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        return super(DynamicFormView2, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return super(DynamicFormView2, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super(DynamicFormView2, self).post(request, *args, **kwargs)
+
+    def get_form_class(self):
+        return super(DynamicFormView2, self).get_form_class()
+
+    def get_form(self, form_class=None):
+        return super(DynamicFormView2, self).get_form(form_class)
+
+    def get_form_kwargs(self, **kwargs):
+        return super(DynamicFormView2, self).get_form_kwargs(**kwargs)
+
+    def get_initial(self):
+        return super(DynamicFormView2, self).get_initial()
+
+    def form_invalid(self, form):
+        return super(DynamicFormView2, self).form_invalid(form)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.save()
+        return super(DynamicFormView2, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        ret = super(DynamicFormView2, self).get_context_data(**kwargs)
+        return ret
+
+    def render_to_response(self, context, **response_kwargs):
+        return super(DynamicFormView2, self).render_to_response(context, **response_kwargs)
+
+    def get_template_names(self):
+        return super(DynamicFormView2, self).get_template_names()
+
+    def get_success_url(self):
+        return reverse("core:criteria_detail", args=(self.object.pk,))
+
+    
+#     def get(self, request, *args, **kwargs):
+#         form = DynamicForm()
+#         return render(request, self.template_name, {'form': form})
+#     
+#     def post(self, request, *args, **kwargs):
+#         form = DynamicForm(request.POST)
+#         
+#         if form.is_valid():
+#             data = request.POST.copy()
+#             form = DynamicForm()
+#             
+#         if request.method == 'POST':
+#             form = DynamicForm(request.POST)
+            
+        
+#         args = {'form': form, 'data':data}
+#         models.Criteria.objects.all()
+#         models.Scale.objects.all()
+#         print(request.POST)
+#         return render(request, self.template_name, args)
